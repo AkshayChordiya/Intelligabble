7635d7376cacb91559c01fc934ae6b8342c16668
Janhavi Dahihande <janhavidahihande@gmail.com>
2020-10-10 14:51:08 -0700

    Add Jetpack compose dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha13")
     }
 }
 group = "aj.intelligabble"
