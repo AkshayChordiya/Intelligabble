fc2b26c4584d897097c69c26dbe5f07bf7aa9a80
Janhavi Dahihande <janhavidahihande@gmail.com>
2020-10-10 14:53:17 -0700

    Add Jetpack Compose UI for Android

--- androidApp/src/main/java/aj/intelligabble/android/MainActivity.kt
+++ androidApp/src/main/java/aj/intelligabble/android/MainActivity.kt
@@ -3,14 +3,36 @@ package aj.intelligabble.android
 import aj.intelligabble.IntelligabbleDataSource
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.Divider
+import androidx.compose.material.ListItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO: Design the UI in Jetpack Compose
-        setContentView(R.layout.activity_main)
-        // TODO: Show the data on UI
         val data = IntelligabbleDataSource.getData()
+        setContent {
+            geekTermsList(data)
+        }
+    }
+
+    @Composable
+    fun geekTermsList(data: Map<String, String>) {
+        LazyColumnFor(data.entries.toList()) { (term, description) ->
+            ListItem(
+                text = {
+                    Text(term)
+                },
+                secondaryText = {
+                   Text(description)
+                }
+            )
+            Divider(color = Color(0x11000000))
+        }
     }
 }
