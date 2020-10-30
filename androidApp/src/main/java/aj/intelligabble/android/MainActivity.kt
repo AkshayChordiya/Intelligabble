59cfbada24b96a56f66fc12e6b9fcf2618ab5f91
Janhavi Dahihande <janhavidahihande@gmail.com>
2020-10-29 20:04:17 -0700

    Add search bar

--- androidApp/src/main/java/aj/intelligabble/android/MainActivity.kt
+++ androidApp/src/main/java/aj/intelligabble/android/MainActivity.kt
@@ -2,18 +2,36 @@ package aj.intelligabble.android
 
 import aj.intelligabble.IntelligabbleDataSource
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ContentGravity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.Divider
 import androidx.compose.material.ListItem
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,16 +46,26 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun geekTermsList(data: Map<String, String>) {
-        LazyColumnFor(data.entries.toList()) { (term, description) ->
-            ListItem(
-                text = {
-                    Text(term)
-                },
-                secondaryText = {
-                   Text(description, modifier = Modifier.padding(vertical = 4.dp))
-                },
+        Column(Modifier.padding(16.dp)) {
+            val textState = remember { mutableStateOf(TextFieldValue()) }
+            TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = textState.value,
+                    onValueChange = { textState.value = it },
+                    label = { Text("Search") },
+                    leadingIcon = { Icon(Icons.Filled.Search) },
             )
-            Divider(color = Color(0x11000000))
+            LazyColumnFor(data.entries.toList()) { (term, description) ->
+                ListItem(
+                        text = {
+                            Text(term)
+                        },
+                        secondaryText = {
+                            Text(description, modifier = Modifier.padding(vertical = 4.dp))
+                        },
+                )
+                Divider(color = Color(0x11000000))
+            }
         }
     }
 }
